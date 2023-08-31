ad19da279a5b0d3fc03ccd35130369076477d7cc
Borislav Sevcik <sm.borislav@gmail.com>
2023-09-01 00:59:12 +0200

    print

--- print_service/app.py
+++ print_service/app.py
@@ -1,6 +1,8 @@
 import time
+import logging
 print("Boracar")
 print("newprint")
-for i in range(30):
-    print("waiting x seconds")
+
+for i in range(10):
+    logging.info('This is an info message')
     time.sleep(1)
