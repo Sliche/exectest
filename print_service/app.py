ab6271f008ca14f914a6d62a7e3f53340e388c1d
Borislav Sevcik <sm.borislav@gmail.com>
2023-09-06 22:56:52 +0200

    pringin

--- print_service/app.py
+++ print_service/app.py
@@ -4,7 +4,7 @@ print("Boracar")
 print("newprint")
 
 for i in range(30):
-    print("waiting x seconds", flush=True)
+    print("waiting "+str(i)+ " seconds", flush=True)
     # x = a + b
     time.sleep(1)
 
