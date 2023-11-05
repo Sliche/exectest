6c2f4798656ac3eadc7c3ff0b5a83e9dd9059da1
Borislav Sevcik <sm.borislav@gmail.com>
2023-11-05 16:26:54 +0100

    new

--- ex_ef/app.py
+++ ex_ef/app.py
@@ -1,11 +1,21 @@
 from time import sleep
+from fx_ef import context
 
 x = 1
 
 while True:
 
+    print("-" * 50)
     print(x)
+    context.logging.debug("debug msg")
+    context.logging.info("info msg")
+    context.logging.error("error msg")
+    context.logging.warning("warning msg")
+    context.logging.critical("critical msg")
+    print("-"*50)
+
     x += 1
     sleep(1)
+
     if x == 100:
         break
