c86fef3253d39ee642d067dce55b67f4aa31adc9
Borislav Sevcik <sm.borislav@gmail.com>
2024-03-05 13:33:08 +0100

    uploading file

--- storage_tests/app.py
+++ storage_tests/app.py
@@ -3,18 +3,19 @@ from time import sleep
 from fx_ef import context
 
 
-print("creating bucket")
-context.storage.create_bucket("borabucka")
-print("bucket created")
+print("uploading file")
+# context.storage.create_bucket("borabucka")
+context.storage.upload("borabucka", "/app/launch.sh")
+print("file uploaded ")
 
 print("-" * 50)
 print("borka korka 1")
 print("-"*50)
 
-my_result_log = {
-    "result1": "somerka",
-    "result2": "somerka2",
-    "result3": "somerka3",
-    "result4": "jugoslovenka"
-}
+# my_result_log = {
+#     "result1": "somerka",
+#     "result2": "somerka2",
+#     "result3": "somerka3",
+#     "result4": "jugoslovenka"
+# }
 
