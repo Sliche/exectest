28cc048595654bc0ee2eab2dd04f6d801b4853fc
Borislav Sevcik <sm.borislav@gmail.com>
2024-05-07 11:32:09 +0200

    a

--- ex_ef/app.py
+++ ex_ef/app.py
@@ -15,26 +15,12 @@ while True:
     # test_state = context.state.get()
     # print(test_state, flush=True)
 
-    print("testing EVENTS functionality", flush=True)
-    context.events.send("event_type_test", "event_source_my_package", data={"test1":"test2"}, topic="ferris.events", reference_id="ref_id_test")
+    # print("testing EVENTS functionality", flush=True)
+    # context.events.send("event_type_test", "event_source_my_package", data={"test1":"test2"}, topic="ferris.events", reference_id="ref_id_test")
 
 
-    # context.result.save(my_result_log)
-
-    # context.events.send(
-    #     event_type="ferris_executor.save_users_logs",
-    #     data={
-    #         "fxcid": "9ccdac99-ce50-4336-a9c4-ce63d18484ef",
-    #         "package_name": context.params["package_name"],
-    #         "result": my_result_log,
-    #         "last_log": False
-    #     }
-    # )
-
-    # context.output.log("My Custom made logs")
-    # context.output.log("My Custom made logs")
-    # context.output.log("My Custom made logs")
-    # context.output.log("My Custom made logs", last_log=True)
+    print("testing EXEC RESULTS functionality", flush=True)
+    context.result.save({"my_result": "saved"})
 
     x += 1
     sleep(5)
