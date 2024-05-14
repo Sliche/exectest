1fead93ae02867f5896245133c224e1ac2d63e69
Borislav Sevcik <sm.borislav@gmail.com>
2024-05-14 15:10:57 +0200

    a

--- ex_ef_events/app.py
+++ ex_ef_events/app.py
@@ -7,7 +7,7 @@ x = 1
 while True:
 
     print("testing EVENTS functionality", flush=True)
-    context.events.send("event_type_test", "event_source_my_package", data={"test1":"test2"}, topic="ferris.events", reference_id="ref_id_test")
+    context.events.send("event_type_test", "event_source_my_package", data={"test1":"test2"}, reference_id="ref_id_test")
 
     x += 1
     sleep(5)
