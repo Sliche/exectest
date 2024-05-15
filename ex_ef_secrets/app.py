42405e50e4784c3e8aebaf45c1244c97e1ef9848
Borislav Sevcik <sm.borislav@gmail.com>
2024-05-15 13:10:58 +0200

    a

--- ex_ef_secrets/app.py
+++ ex_ef_secrets/app.py
@@ -8,7 +8,9 @@ while True:
 
     print("testing SECRETs functionality", flush=True)
     bora_sec = context.secrets.get("testsecret")
+    print(bora_sec)
     bora_set = context.secrets.set("testsecret", "value", "context")
+    
 
     x += 1
     sleep(5)
